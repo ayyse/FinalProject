a056c48696234616c70398d433769facde5e256c
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-03-01 01:29:09 +0300

    EfCore

--- FinalProject/DataAccess/Abstract/IEntityRepository.cs
+++ FinalProject/DataAccess/Abstract/IEntityRepository.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    // Generic Constraint: T referans tipinde veya IEntity veya IEntity implemente eden bir nesne olabilir 
+    // where T : class, IEntity => buradaki class sınıfı değil referans tipi ifade eder
+    // new() => newlenebilir olmayı ifade eder new() kullanarak IEntity saf dışı bırakılır
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll();
+        // Belirli bir datayı çekebilmek için Linq Expression kullanılır
+        // Kullanıcı filtreleme istemezse tüm datayı getirebilmesi için filter = null verilir
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int categoryId);
     }
 }
