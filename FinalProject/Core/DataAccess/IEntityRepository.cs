f4c2595d1963d534f2fdfd35c018e85b5e469259
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-03-25 10:29:05 +0300

    ders-9

--- FinalProject/Core/DataAccess/IEntityRepository.cs
+++ FinalProject/Core/DataAccess/IEntityRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // Generic Constraint: T referans tipinde veya IEntity veya IEntity implemente eden bir nesne olabilir 
     // where T : class, IEntity => buradaki class sınıfı değil referans tipi ifade eder
--- FinalProject/DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ FinalProject/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,55 +11,24 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
+        public List<ProductDetailDto> GetProductDetails()
         {
-            // IDisposable pattern implementation of c#
-            using (NorthwindContext northwindContext = new NorthwindContext())
+            using (NorthwindContext context = new NorthwindContext())
             {
-                // referans alma
-                var addedEntity = northwindContext.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                northwindContext.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext northwindContext = new NorthwindContext())
-            {
-                // referans alma
-                var deletedEntity = northwindContext.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                northwindContext.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext northwindContext = new NorthwindContext())
-            {
-                return northwindContext.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext northwindContext =  new NorthwindContext())
-            {
-                return filter == null ? northwindContext.Set<Product>().ToList() : northwindContext.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext northwindContext = new NorthwindContext())
-            {
-                // referans alma
-                var updatedEntity = northwindContext.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                northwindContext.SaveChanges();
+                // result IQuerable olduğu için returnda ToList ile dönmesi gerekti
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
             }
         }
     }
