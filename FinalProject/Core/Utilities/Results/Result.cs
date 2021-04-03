226b2c2819a43182f44746622a0ff95a2a0dd6d5
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-04-04 00:13:41 +0300

    ders-12

---
+++ FinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // dont repeat yourself 
+        public Result(bool success, string message) : this(success) // this ile alttaki constr. base alındı
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
